fd48d8ad086ff1918c9b5e6fcc3c366be249a728
Luis Pezo <lpezo777@gmail.com>
2020-01-18 10:39:22 -0500

    change environment production

--- server/Utilities/config.js
+++ server/Utilities/config.js
@@ -7,10 +7,9 @@ let serverURLs = {
         "MONGO_DB": "mongodb://localhost:27017/angular-login-register"
     },
     "production": {
-        "NODE_SERVER": process.env.HOST,
-        "NODE_SERVER_PORT": process.env.PORT,
-        "MONGO_DB": process.env.MONGO_URI || "mongodb://localhost:27017/angular-login-register"
-
+        "NODE_SERVER": process.env.HOST || "http://localhost",
+        "NODE_SERVER_PORT": process.env.PORT || 3000,
+        "MONGO_DB": process.env.MONGO_URI || "mongodb://xmls:ASCiraWyrtSm87Wc7jDOF1NUaoW1cyQ389dl8GFDoyuvowFbPrmbdN5YvwpnYquPhtdWA3OJSM2pYGdZjnLpOw==@xmls.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
     }
 }
 
