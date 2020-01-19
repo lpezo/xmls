0e562f0c44d65c9af500d3399f3d177f1dad0c9d
Luis Pezo <lpezo777@gmail.com>
2020-01-19 09:26:29 -0500

    fix for cosmos

--- server/Utilities/config.js
+++ server/Utilities/config.js
@@ -1,21 +1,29 @@
+var env = require('dotenv').config();
 let environment = process.env.NODE_ENV || "dev";
 
 let serverURLs = {
     "dev": {
         "NODE_SERVER": "http://localhost",
         "NODE_SERVER_PORT": "3000",
-        "MONGO_DB": "mongodb://localhost:27017/angular-login-register"
+        "MONGO_DB": "mongodb://localhost:27017/angular-login-register",
+        "MONGO_USER": null,
+        "MONGO_PWD": null
     },
     "production": {
         "NODE_SERVER": process.env.HOST || "http://localhost",
         "NODE_SERVER_PORT": process.env.PORT || 3000,
-        "MONGO_DB": process.env.MONGO_URI || "mongodb://xmls:ASCiraWyrtSm87Wc7jDOF1NUaoW1cyQ389dl8GFDoyuvowFbPrmbdN5YvwpnYquPhtdWA3OJSM2pYGdZjnLpOw==@xmls.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+        "MONGO_DB": "mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb",
+        "MONGO_USER": process.env.COSMODDB_USER,
+        "MONGO_PWD": process.env.COSMOSDB_PASSWORD        
     }
 }
+        //"mongodb://xmls:ASCiraWyrtSm87Wc7jDOF1NUaoW1cyQ389dl8GFDoyuvowFbPrmbdN5YvwpnYquPhtdWA3OJSM2pYGdZjnLpOw==@xmls.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
 
 let config = {
     "DB_URL": {
-        "url": `${serverURLs[environment].MONGO_DB}`
+        "url": `${serverURLs[environment].MONGO_DB}`,
+        "user": `${serverURLs[environment].MONGO_USER}`,
+        "pwd": `${serverURLs[environment].MONGO_PWD}`
     },
     "NODE_SERVER_PORT": {
         "port": `${serverURLs[environment].NODE_SERVER_PORT}`
