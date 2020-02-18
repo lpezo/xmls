355cf06d6499438fc8eb574b5618605aa0426471
Luis Pezo <lpezo@canvia.com>
2020-02-17 20:11:53 -0500

    fix send mail

--- server/Utilities/mail.js
+++ server/Utilities/mail.js
@@ -2,6 +2,7 @@ var nodemailer = require('nodemailer');
 var userDao = require('../DAO/userDAO');
 var fs = require('fs');
 
+/*
 var getAuth = () => {
     let scred = fs.readFileSync('./credentials.json');
     let cred = JSON.parse(scred);
@@ -23,14 +24,30 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: getAuth()
    });
+*/
+
+var smtpConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL, 
+                  // you can try with TLS, but port is then 587
+    auth: {
+      user: 'lpezo777@gmail.com', // Your email id
+      pass: 'enotriaweb01' // Your password
+    }
+  };
 
 const mailOptions = {
-    from: 'myxml@gmail.com',
+    from: 'lpezo777@gmail.com',
     subject: '[proceso %name%] aviso de termino de proceso',
-    html: '<p>El proceso del proyecto []%name%] ya ha terminado, puede descargar el excel desdel la pagina web del aplicativo.</p>'
+    html: '<p>El proceso del proyecto [%name%] ya ha terminado, puede descargar el excel desdel la pagina web del aplicativo.</p>'
 };
 
 const sendAvisoFin = (proy) => {
+
+    var transporter = nodemailer.createTransport(smtpConfig);
+    // replace hardcoded options with data passed (somedata)
+
     return new Promise((resolve, reject)=>{
         userDao.getUser(proy.user).then(user=>{
             mailOptions.to = user.email;
